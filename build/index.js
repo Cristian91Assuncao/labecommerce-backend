a60e6bc6296d4a2579cc749d2973e0f40cd21180
Cristian91Assuncao <cristian.assuncao@hotmail.com>
2023-09-05 12:14:48 -0300

    Exercícios da aula de typescript I

---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+const setTimeout = () => {
+    console.table(database_1.users);
+    console.table(database_1.products);
+};
+setTimeout();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { users, products } from "./database";
+
+const setTimeout = () => {
+    console.table(users);
+    console.table(products);
+};
+
+setTimeout()
