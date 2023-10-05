3e6048b7663690d0b43b3ff7502fdc5d86f80ad7
Amanda Gonçalves <cristian.assuncao@hotmail.com>
2023-10-05 14:14:38 -0300

    resolução dos exercícios da aula de introducao a sql

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,25 +1,24 @@
--- Active: 1695771350690@@127.0.0.1@3306
-
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created_at TEXT DEFAULT(DATETIME('now', 'localtime'))
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL
 );
 
-INSERT INTO users ( id, name, email, password )
-VALUES ( 'u01', 'Cartman', 'cartman@email.com', '123456' );
-
-INSERT INTO users ( id, name, email, password )
-VALUES ( 'u02', 'Kyle', 'kyle@email.com', '123456' );
-
-INSERT INTO users ( id, name, email, password )
-VALUES ( 'u03', 'Kenny', 'kennycartman@email.com', '123456' );
-
-SELECT * FROM users
+SELECT * FROM users;
 
 DROP TABLE users;
+
+INSERT INTO users ( id, name, email, password )
+VALUES ( 'us001', 'Cartman', 'cartman@email.com', '123456' );
+
+INSERT INTO users ( id, name, email, password )
+VALUES ( 'us002', 'Kenny', 'kenny@email.com', '123456' );
+
+INSERT INTO users ( id, name, email, password )
+VALUES ( 'us003', 'Stan', 'stan@email.com', '123456' );
+
 
 CREATE TABLE products (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
@@ -27,16 +26,21 @@ CREATE TABLE products (
     price REAL NOT NULL,
     description TEXT NOT NULL,
     image_url TEXT NOT NULL
-)
+);
+
+SELECT * FROM products;
 
 INSERT INTO products ( id, name, price, description, image_url )
-VALUES
-('p01', 'Cama', 900, 'Cama King Size', 'imagem'),
-('p02', 'Sofá', 1500, 'Sofá de Canto', 'imagem'),
-('p03', 'Mesa', 700, 'Mesa de cozinha', 'imagem'),
-('p04', 'Guarda-roupa', 1000, 'Guarda-roupa 8 portas', 'imagem'),
-('p05', 'Rack', 400, 'Rack para TV 50 polegadas', 'imagem')
+VALUES ( 'pro001', 'Notebook', 3500, 'Notebook Dell', 'image' );
 
-SELECT * FROM products
+INSERT INTO products ( id, name, price, description, image_url )
+VALUES ( 'pro002', 'Cadeira Gamer', 999, 'Cadeira super confortável', 'image' );
 
-DROP TABLE products
+INSERT INTO products ( id, name, price, description, image_url )
+VALUES ( 'pro003', 'Escrivaninha', 650, 'Escrivaninha de escritório', 'image' );
+
+INSERT INTO products ( id, name, price, description, image_url )
+VALUES ( 'pro004', 'Fone Bluetooth', 120, 'Fone bluetooth com som fodaaa', 'image' );
+
+INSERT INTO products ( id, name, price, description, image_url )
+VALUES ( 'pro005', 'Teclado Bluetooth', 150, 'Teclado liso e resistente', 'image' );
